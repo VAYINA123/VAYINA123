82159a98a96c4472f7b1a0b0041e556106e7f3dd
VAYINA123 <tokoisimon1945@gmail.com>
2024-10-28 16:00:53 +0400

    first commit

--- Day5/HW/LVL5.py
+++
@@ -1,6 +0,0 @@
-Num0 = (int(input = "Enter Number: "))
-Num1 = (int(input = "Enter Number: "))
-Num2 = (int(input = "Enter Number: "))
-Num3 = (int(input = "Enter Number: "))
-
-print (Num0 + Num1 * Num2 // Num3)
