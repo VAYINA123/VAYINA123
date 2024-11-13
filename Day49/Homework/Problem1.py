370018b01528639ad220f0ed979516b42c633810
VAYINA123 <tokoisimon1945@gmail.com>
2024-11-13 17:29:07 +0400

    first commit

---
+++ Day49/Homework/Problem1.py
@@ -0,0 +1,28 @@
+# students = {"Ana": 80, "Beka": 90, "Dato": 85}  # სტუდენტების ნიშნების მაგალითი
+# best_student = max(students, key=students.get)
+# print(f"საუკეთესო სტუდენტი: {best_student}, ნიშანი: {students[best_student]}")
+
+
+
+
+# grades = [78, 85, 90, 67, 92]  # ქულების მაგალითი
+# max_grade = max(grades)
+# min_grade = min(grades)
+# avg_grade = sum(grades) / len(grades)
+# print(f"მაქსიმალური ქულა: {max_grade}, მინიმალური ქულა: {min_grade}, საშუალო ქულა: {avg_grade}")
+
+
+
+
+
+# numbers = [1, 2, 3, 4, 5]  # რიცხვების მაგალითი
+# reversed_numbers = numbers[::-1]
+# print(f"შებრუნებული სია: {reversed_numbers}")
+
+
+
+
+
+# numbers = [10, 20, 30, 40, 50]  # რიცხვების მაგალითი
+# average = sum(numbers) / len(numbers)
+# print(f"საშუალო მნიშვნელობა: {average}")
---
+++ Day53/index.py
@@ -0,0 +1,2 @@
+#https://www.frontendmentor.io/challenges/social-links-profile-UG32l9m6dQ
+# https://www.frontendmentor.io/challenges/nft-preview-card-component-SbdUL_w0U
